3c322ef07606361b842681ebc98cdab6fe64ecad
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2022-01-24 13:47:02 -0300

    Conserto

--- db_connect.php
+++ db_connect.php
@@ -3,6 +3,6 @@
 $connect_url = getenv("postgres://layrcwwccwepoy:019295c77e48ace2aa14e61028e66f9d937be8d5277d7587e6eca553dffe47c8@ec2-23-23-128-222.compute-1.amazonaws.com:5432/d7orfdv4son25g");
 $connect = pg_connect($connect_url);
 
-echo "Ok";
+echo gettype($connect);
 
 ?>
