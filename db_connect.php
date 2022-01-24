8a304bee4e486a3257861d04d4526f3cf011d0b5
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2022-01-23 22:50:10 -0300

    Conserto

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ $servername = "
 postgres://layrcwwccwepoy:019295c77e48ace2aa14e61028e66f9d937be8d5277d7587e6eca553dffe47c8@ec2-23-23-128-222.compute-1.amazonaws.com:5432/d7orfdv4son25g"; //endereço do servidor
 $username="layrcwwccwepoy";
 $password="019295c77e48ace2aa14e61028e66f9d937be8d5277d7587e6eca553dffe47c8";
-$db_name="Plantoterapia";
+$db_name="d7orfdv4son25g";
 
 //pdo - somente orientado objeto
 $connect = pg_connect($servername,$username,$password,$db_name);
