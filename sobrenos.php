9fe9328cb8650e52637c5827d61c8d1d4e910136
jessicaduque <69450467+jessicaduque@users.noreply.github.com>
2022-01-24 12:15:40 -0300

    Conserto

--- sobrenos.php
+++ sobrenos.php
@@ -2,7 +2,7 @@
 <html>
 
     <head>
-        <link href="css/styles.css" rel="stylesheet">
+        <link href="styles.css" rel="stylesheet">
         <meta charset="utf-8">
         <title> Plantoterapia </title>
     </head>
@@ -15,22 +15,22 @@
         <div class="sobrenos">
 
                 <div class="pessoa"> 
-                    <div class="imagem"> <img src="imagem\equipe\nauvia.jpeg"> </div>
+                    <div class="imagem"> <img src="nauvia.jpeg"> </div>
                     <div class="texto"> <p> Nauvia Maria Cancelieri é professora de química no IFES Campus Serra e colaboradora do projeto. </p> </div>
                 </div>
 
                 <div class="pessoa"> 
-                    <div class="imagem"> <img src="imagem\equipe\amanda.jpeg"> </div>
+                    <div class="imagem"> <img src="amanda.jpeg"> </div>
                     <div class="texto"> <p> Amanda de Góes Pires é estudante do IFES Campus Serra, possui por volta dos 18 anos e é uma das desenvolvedoras desse projeto. </p> </div>
                 </div>
 
                 <div class="pessoa"> 
-                    <div class="imagem"> <img src="imagem\equipe\jessica.jpeg"> </div>
+                    <div class="imagem"> <img src="jessica.jpeg"> </div>
                     <div class="texto"> <p> Jessica Nogueira Duque é estudante do IFES Campus Serra, possui por volta dos 17 anos e é uma das desenvolvedoras desse projeto. </p> </div>
                 </div>
 
                 <div class="pessoa"> 
-                    <div class="imagem"> <img src="imagem\equipe\sophia.jpeg"> </div>
+                    <div class="imagem"> <img src="sophia.jpeg"> </div>
                     <div class="texto"> <p> Sophia Caetano Marques Souza é estudante do IFES Campus Serra, possui por volta dos 18 anos e é uma das desenvolvedoras desse projeto. </p> </div>
                 </div>
             
